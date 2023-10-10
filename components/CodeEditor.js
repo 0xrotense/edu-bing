0b111fd37659388d0afcbaf0ec66c62ef4b7d5c4
0x0god <rotense@gmail.com>
2023-10-10 23:50:04 +0100

    import required modules

---
+++ components/CodeEditor.js
@@ -0,0 +1,6 @@
+// Importing required modules
+import { StreamLanguage } from '@codemirror/language';
+import { go } from '@codemirror/legacy-modes/mode/go';
+import CodeMirror from '@uiw/react-codemirror';
+import { useEffect, useState } from 'react';
+import { eclipse } from "@uiw/codemirror-theme-eclipse";

588598e0f0dc15d3b723e7d17af18dc68746d7e7
0x0god <rotense@gmail.com>
2023-10-10 23:38:18 +0100

    added CodeEditor component for inbrowser editing

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0302d366a0a21dc5"; }
+/******/ 		__webpack_require__.h = function() { return "6807ced289102e15"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
